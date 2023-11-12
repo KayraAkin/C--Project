a676a6e24fc8feb23a0b31950053bdddcb3a72ed
KayraAkin <kayrakin11@hotmail.com>
2023-11-12 18:01:56 +0300

    deneme

---
+++ ConsoleApp1/Matematik/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dortıslem dortıslem = new Dortıslem();
+
+            dortıslem.Topla(5, 6);
+            dortıslem.Topla(8, 10);
+
+        }
+    }
+}
---
+++ ConsoleApp1/Metotlar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler Sepete Eklendi:  " + urun.Adi);
+        }
+
+    }
+}
